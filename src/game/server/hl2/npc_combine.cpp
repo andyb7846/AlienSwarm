efab8441020c7f82b4a7f26e89ba88ffa1c4d71e
Andrew Brown <47788622+andyb7846@users.noreply.github.com>
2020-06-18 23:13:19 +0100

    Changed NPC's vision cone 0.5f to ULTRA_NARROW
    The dot products don't work like we think they do, or at least I can't figure them out, luckily enough valve have made a non-dot product string which correlates to a value, we can now sneak around the mercenaries! Bueno!

--- src/game/server/hl2/npc_combine.cpp
+++ src/game/server/hl2/npc_combine.cpp
@@ -412,7 +412,7 @@ void CNPC_Combine::Spawn( void )
 	AddSolidFlags( FSOLID_NOT_STANDABLE );
 	SetMoveType( MOVETYPE_STEP );
 	SetBloodColor( BLOOD_COLOR_RED );
-	m_flFieldOfView			= -0.5f;// indicates the width of this NPC's forward view cone ( as a dotproduct result )
+	m_flFieldOfView			= -ULTRA_NARROW;// indicates the width of this NPC's forward view cone ( as a dotproduct result )
 	m_NPCState				= NPC_STATE_NONE;
 	m_flNextGrenadeCheck	= gpGlobals->curtime + 1;
 	m_flNextPainSoundTime	= 0;
